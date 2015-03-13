111c52558951f3e9d742a5218d566701c7558691
Alberto Donato <alberto.donato@gmail.com>
2015-03-13 22:58:29 +0200

    Add parsing for process files.

--- procsys/parse/proc.py
+++ procsys/parse/proc.py
@@ -1,3 +1,18 @@
+#
+# This file is part of Procsys.
+
+# Procsys is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# Procsys is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Procsys.  If not, see <http://www.gnu.org/licenses/>.
+
 '''Parsers for Linux /proc files.'''
 
 from procsys.parse.text import FileParser, SingleLineFileParser
@@ -64,3 +79,66 @@ class ProcDiskstats(FileParser):
             values = [int(value) for value in values]
             result[dev_name] = dict(zip(self.diskstat_fields, values))
         return result
+
+
+class ProcPIDStat(SingleLineFileParser):
+    '''Parser for /proc/[pid]/stat and /proc/[pid]/tasks/[tid]/stat.'''
+
+    fields = (
+        None,
+        None,
+        ('state', str),
+        ('ppid', int),
+        ('pgrp', int),
+        ('session', int),
+        ('tty_nr', int),
+        ('tpgid', int),
+        ('flags', int),
+        ('minflt', int),
+        ('cminflt', int),
+        ('majflt', int),
+        ('cmajflt', int),
+        ('utime', int),
+        ('stime', int),
+        ('cutime', int),
+        ('cstime', int),
+        ('priority', int),
+        ('nice', int),
+        ('num_threads', int),
+        ('itrealvalue', int),
+        ('starttime', int),
+        ('vsize', int),
+        ('rss', int),
+        ('rsslim', int),
+        ('startcode', int),
+        ('endcode', int),
+        ('startstack', int),
+        ('kstkesp', int),
+        ('kstkeip', int),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        ('exit_signal', int),
+        ('processor', int),
+        ('rt_priority', int),
+        ('policy', int),
+        ('delayacct_blkio_ticks', int),
+        ('guest_time', int),
+        ('cguest_time', int))
+
+
+class ProcPIDStatm(SingleLineFileParser):
+    '''Parser for /proc/[pid]/statm.'''
+
+    fields = (
+        ('size', int),
+        ('resident', int),
+        ('share', int),
+        ('text', int),
+        ('lib', int),
+        ('data', int),
+        ('dt', int))
--- procsys/parse/tests/test_proc.py
+++ procsys/parse/tests/test_proc.py
@@ -1,7 +1,23 @@
+#
+# This file is part of Procsys.
+
+# Procsys is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# Procsys is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Procsys.  If not, see <http://www.gnu.org/licenses/>.
+
 from procsys.testing import TestCase
 
 from procsys.parse.proc import (
-    ProcStat, ProcUptime, ProcLoadavg, ProcVmstat, ProcDiskstats)
+    ProcStat, ProcUptime, ProcLoadavg, ProcVmstat, ProcDiskstats,
+    ProcPIDStat, ProcPIDStatm)
 
 
 class ProcStatTests(TestCase):
@@ -98,3 +114,61 @@ class ProcDiskstatsTest(TestCase):
                 'read': 1, 'read-merged': 2, 'read-sect': 3, 'read-ms': 4,
                 'write': 5, 'write-merged': 6, 'write-sect': 7, 'write-ms': 8,
                 'io-curr': 9, 'io-ms': 10, 'io-ms-weighted': 11}})
+
+
+class ProcPIDStatTests(TestCase):
+
+    def test_fields(self):
+        '''Fields and values from /proc/[pid]/stat files are reported.'''
+        content = ' '.join(str(i) for i in range(45))
+        path = self.mkfile(content=content)
+        parser = ProcPIDStat(path)
+        self.assertEqual(
+            parser.parse(),
+            {'state': '2',
+             'ppid': 3,
+             'pgrp': 4,
+             'session': 5,
+             'tty_nr': 6,
+             'tpgid': 7,
+             'flags': 8,
+             'minflt': 9,
+             'cminflt': 10,
+             'majflt': 11,
+             'cmajflt': 12,
+             'utime': 13,
+             'stime': 14,
+             'cutime': 15,
+             'cstime': 16,
+             'priority': 17,
+             'nice': 18,
+             'num_threads': 19,
+             'itrealvalue': 20,
+             'starttime': 21,
+             'vsize': 22,
+             'rss': 23,
+             'rsslim': 24,
+             'startcode': 25,
+             'endcode': 26,
+             'startstack': 27,
+             'kstkesp': 28,
+             'kstkeip': 29,
+             'exit_signal': 37,
+             'processor': 38,
+             'rt_priority': 39,
+             'policy': 40,
+             'delayacct_blkio_ticks': 41,
+             'guest_time': 42,
+             'cguest_time': 43})
+
+
+class ProcPIDStatmTests(TestCase):
+
+    def test_fields(self):
+        '''Fields and values from /proc/[pid]/statm files are reported.'''
+        path = self.mkfile(content='1 2 3 4 5 6 7')
+        parser = ProcPIDStatm(path)
+        self.assertEqual(
+            parser.parse(),
+            {'size': 1, 'resident': 2, 'share': 3, 'text': 4, 'lib': 5,
+             'data': 6, 'dt': 7})
