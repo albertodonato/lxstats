b803b38100ef6dbfcd1c0276a74d614b0a47ec18
Alberto Donato <alberto.donato@gmail.com>
2017-03-05 12:58:13 +0100

    Drop headers.

--- lxstats/files/__init__.py
+++ lxstats/files/__init__.py
@@ -1,18 +1,3 @@
-#
-# This file is part of LxStats.
-#
-# LxStats is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# LxStats is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# LxStats.  If not, see <http://www.gnu.org/licenses/>.
-
 '''Read and write files under :file:`/proc` and :file:`/sys` Linux filesystems.
 
 This module and its submoduels contain specialized classes to read, parse and
