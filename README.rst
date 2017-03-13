4209036d1debaa1f36a76e0d9608beb76e26f72f
Alberto Donato <alberto.donato@gmail.com>
2017-03-13 14:54:17 +0100

    Update readme and license.

--- README.rst
+++ README.rst
@@ -2,8 +2,8 @@
 LxStats
 =======
 
-LxStats provides access to system and processes statistics and metrics on Linux
-systems.
+LxStats is a library to access system and process statistics and metrics on
+Linux systems.
 
-It provides easy access and parsing of information from various files under
-the /proc and /sys filesystems.
+It provides easy parsing of information from various files under the ``/proc``
+and ``/sys`` filesystems.
--- setup.py
+++ setup.py
@@ -6,28 +6,27 @@ from lxstats import __version__, __doc__ as description
 config = {
     'name': 'lxstats',
     'version': __version__,
-    'license': 'GPLv3+',
+    'license': 'LGPLv3+',
     'description': description,
     'long_description': open('README.rst').read(),
     'author': 'Alberto Donato',
     'author_email': 'alberto.donato@gmail.com',
     'maintainer': 'Alberto Donato',
     'maintainer_email': 'alberto.donato@gmail.com',
-    'url': 'https://bitbucket.org/ack/lxstats',
-    'download_url': 'https://bitbucket.org/ack/lxstats/downloads',
+    'url': 'https://github.com/albertodonato/lxstats',
     'packages': find_packages(),
     'include_package_data': True,
     "entry_points": {
         "console_scripts": ["procs = lxstats.scripts.procs:script"]},
     'test_suite': 'lxstats',
-    'install_requires': ['prettytable', 'toolrack >= 1.0.1'],
+    'install_requires': ['prettytable', 'toolrack'],
     'keywords': 'linux proc sys performance monitoring system admin',
     'classifiers': [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: System Administrators',
         ('License :: OSI Approved :: '
-         'GNU General Public License v3 or later (GPLv3+)'),
+         'GNU Lesser General Public License v3 or later (LGPLv3+)'),
         'Operating System :: POSIX :: Linux'
         'Operating System :: Unix',
         'Programming Language :: Python :: 3.5',
