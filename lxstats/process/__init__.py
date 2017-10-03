b371aa54a638986eb601830ef46e29826c875c72
Alberto Donato <alberto.donato@gmail.com>
2017-10-03 18:00:51 +0200

    Export classes through lxstats.process
    Signed-off-by: Alberto Donato <alberto.donato@gmail.com>

--- lxstats/process/__init__.py
+++ lxstats/process/__init__.py
@@ -1,1 +1,15 @@
 """Access information about running processes."""
+
+from .collection import (
+    Collector,
+    Collection)
+from .process import (
+    Process,
+    Task)
+from .formatter import Formatter
+from .filter import CommandLineFilter
+
+
+__all__ = [
+    'Collector', 'Collection', 'Process', 'Task', 'Formatter',
+    'CommandLineFilter']
